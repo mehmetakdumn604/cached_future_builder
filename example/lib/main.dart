d9b5351f73c184462ccbe372ce962941f172cbf0
mehmetakdumann <mehmetakduman411@gmail.com>
2024-06-10 22:51:36 +0300

    [feature] added example using

---
+++ example/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:cached_future_builder/cached_future_builder.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CachedFutureBuilder(
+        onData: (data) {
+          return Center(
+            child: Text(data.toString()),
+          );
+        },
+        future: Future.value("Hello CachedFutureBuilder"),
+        onWaiting: () {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+        onError: (error) {
+          return Center(
+            child: Text(error.toString()),
+          );
+        },
+        onNoData: () {
+          return const Center(
+            child: Text("No data"),
+          );
+        },
+        cacheManager: LocalCacheManager(
+          cacheKey: "cachedfuturebuilder",
+          enableLogging: true,
+          cacheDuration: const Duration(days: 1),
+        ),
+      ),
+    );
+  }
+}
